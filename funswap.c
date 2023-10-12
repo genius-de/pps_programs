e011d259d40cf72617753f3e2630373cb073dc7a
kerito <devansh28112004@gmail.com>
2023-10-12 22:01:58 +0530

    function to swap to mynber using fuction

---
+++ funswap.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void swap(int *,int *);
+
+
+int main ()
+{
+  int a, b;
+ 
+  printf("Enter two numbers: ");
+  scanf("%d%d", &a, &b);
+
+  printf("Before Swapping : a=%d,b=%d\n",a,b);
+
+  swap(&a,&b);
+
+  printf("After Swapping : a=%d,b=%d\n",a,b);
+  return 0;
+}
+ 
+
+void swap(int *a,int *b){
+    int tmp;
+    tmp = *a;
+    *a=*b;
+    *b=tmp;
+}
