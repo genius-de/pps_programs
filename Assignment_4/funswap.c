467c1656e7d5b5ea62606026ad0ab832c4fbf638
Devansh <Devansh28112004@gmail.com>
2023-12-28 17:39:30 +0530

    huygy

--- Assignment_4/funswap.c
+++ Assignment_4/funswap.c
@@ -1,21 +1,24 @@
-#include<stdio.h>
-int swapo(int a,int b){
-  a=a+b;
-  b=a-b;
-  a=a-b;
-  printf("after returning a= %d & b= %d\n",a,b);
+#include <stdio.h>
+int swapo(int a, int b)
+{
+  a = a + b;
+  b = a - b;
+  a = a - b;
+  printf("after returning a= %d & b= %d\n", a, b);
 }
-int swap(int a,int b){
-  int temp =a;
+int swap(int a, int b)
+{
+  int temp = a;
   a = b;
   b = temp;
-  printf("after returning a= %d & b= %d\n",a,b);
+  printf("after returning a= %d & b= %d\n", a, b);
 }
 int main()
 {
-  int a,b;
+  int a, b;
   printf("please entr a and b \n");
-  scanf("%d %d",&a ,&b);
-  printf("before returning a= %d & b= %d\n",a,b);
-  swap(a,b);
+  scanf("%d %d", &a, &b);
+  jj
+      printf("before returning a= %d & b= %d\n", a, b);
+  swap(a, b);
 }
